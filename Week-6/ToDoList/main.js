5733b3f55ccebc2e0b89381da4d1d232bf9c9ae7
NihatNadir <nihat.nadir.borlu@outlook.com>
2025-01-07 01:25:51 +0300

    localStorage added

--- Week-6/ToDoList/main.js
+++ Week-6/ToDoList/main.js
@@ -1,20 +1,44 @@
+// Local Storage veri ekleme
+function saveLocal(x){
+  const tasks = JSON.parse(localStorage.getIte("toDoList")) || { "tasks": [] };
+  tasks.tasks.push(x);  
+  localStorage.setItem("toDoList",JSON.stringif(tasks));
+}
+  
+// Local Storage veri silme
+function removeLocal(x){
+  const tasks = JSON.parse(localStorage.getIte("toDoList")) || { "tasks": [] };
+
+  tasks.tasks = tasks.tasks.filter(task => {
+    return task !== x
+  })
+  localStorage.setItem("toDoList",JSON.stringif(tasks));
+}
+  
+// Butonlara x işareti ekleme
 window.addEventListener("load",() => {
-    const list = document.querySelectorAll('#list li');
-    list.forEach(li =>{
-        const closeButton = document.createElement("span");
-        closeButton.innerText = "\u00D7";
-        closeButton.classList.add("close");
-        li.appendChild(closeButton);
+    const list = document.querySelectorAll('#list li');   
+    localStorage.setItem("toDoList",JSON.stringify({ "tasks": [] }));
+    list.forEach(li =>{  
+      saveLocal(li.textContent);   
+      const closeButton = document.createElement("span");
+      closeButton.innerText = "\u00D7";
+      closeButton.classList.add("close");
+      li.appendChild(closeButton);       
+        
     })
 });
 
+// Butonlara basınca elementi silme
 window.addEventListener("click", event => {    
     if(event.target.className === 'close'){
-        event.target.parentElement.remove();
+      removeLocal(event.target.parentElement.firstChild.textContent);
+      event.target.parentElement.remove();       
     }
 }
 )
 
+// Yeni eleman ekleme
 function newElement() {
     const taskInput = document.getElementById("task");
     const taskValue = taskInput.value.trim(); 
@@ -28,7 +52,7 @@ function newElement() {
       
       const li = document.createElement("li");
       li.textContent = taskValue;
-    
+      saveLocal(li.textContent);
       const closeButton = document.createElement("span");
       closeButton.textContent = "\u00D7"; // text içeriğine x ekliyoruz
       closeButton.className = "close";         
@@ -37,11 +61,14 @@ function newElement() {
       $(toastSuccess).toast("show");
     }
     taskInput.value = "";
+    
   }
   
- 
-  document.getElementById("list").addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked"); 
-    }
-  });
+ // Üstüne tıklayınca üstü çizili yapma (tamamlanma anlamında)
+document.getElementById("list").addEventListene("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked"); 
+  }
+});
+
+  
